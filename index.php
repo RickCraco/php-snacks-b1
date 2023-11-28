463d4b7355dddb9c7ce33c9d77c1aed949632c41
RickCraco <riccardocracolici43@gmail.com>
2023-11-28 15:34:45 +0100

    aggiunti controlli per verifica dei valori nel form + condizioni per garantire l'accesso

--- index.php
+++ index.php
@@ -24,6 +24,20 @@ foreach($matches as $match){
 
 //snack 2
 
+if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+
+    if(strlen($name) > 3 && filter_var($mail, FILTER_VALIDATE_EMAIL) && is_numeric($age)) {
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }
+} else {
+    echo "Errore: Assicurati di compilare tutti i campi del form.";
+}
+
 
 
 ?>
